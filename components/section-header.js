839c3fabd290fc426b2b80192dfe4383de7ec5fa
Abhi Patel <mr.abhiofficial100@gmail.com>
2022-11-10 18:35:17 +0530

    make it better

--- components/section-header.js
+++ components/section-header.js
@@ -5,12 +5,11 @@ const SectionHeader = (props) => {
     <>
       {
         props.title ?
-          <div className='section-header xs:mt-5'>
+          <div className='section-header xs:mt-5 my-8'>
             <div>
-              <p className='md:text-4xl text-2xl mb-2 flex justify-center'>{ props.title }</p>
+              <p className='md:text-4xl text-2xl mb-2 flex justify-center font-semibold Section-header-title'>{ props.title }</p>
               <div className='section-header-lines'>
                 <div className='line-1'></div>
-                <div className='line-2'></div>
               </div>
             </div>
           </div>
